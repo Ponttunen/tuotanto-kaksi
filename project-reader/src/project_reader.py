deb65240499a00c47609e0afe8e9ea87aa8ec6c5
Ponttunen <mymememail2018@protonmail.com>
2025-11-10 23:16:39 +0200

    Yritetty on mutta jotain tuossa on pielessä kun parsii dictionaryn vaan kahteen keyhyn... Hjälp!

---
+++ project-reader/src/project_reader.py
@@ -0,0 +1,25 @@
+import toml
+from urllib import request
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        print(content)
+        parsed_content = toml.loads(content)        
+        print(len(parsed_content))
+        print(parsed_content)
+        print(parsed_content.keys())
+        print("tool:")
+        print(parsed_content["tool"])
+        print("build-system:")
+        print(parsed_content["build-system"])
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(parsed_content["name"], parsed_content["description"], 
+                       parsed_content["license"], [], [])
